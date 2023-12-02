1ac3cbf48ee6c831590430415f846037d260a3c2
SuperN64 <41817899+supern64@users.noreply.github.com>
2023-12-02 23:02:03 +0700

    attempt

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,1 +1,1 @@
-export const BASE_API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+export const BASE_API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://api.t2h.cirnoslab.me";
